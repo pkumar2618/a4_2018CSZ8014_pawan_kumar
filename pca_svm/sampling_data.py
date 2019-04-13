367929574227056a7ce89f7992bf7a451854e45e
pkumar2618 <pawan.kumar@cse.iitd.ac.in>
2019-04-13 22:24:33 +0530

    sampling data to create a csv file with feature and target

--- pca_svm/sampling_data.py
+++ pca_svm/sampling_data.py
@@ -5,8 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-path ='/Users/pawan/Documents/ml_assg/assig4/train_dataset/00000001/'
+# path ='/Users/pawan/Documents/ml_assg/assig4/train_dataset/00000001/'
+path = '/home/dell/Documents/2nd_sem/ml_assig/train_dataset/00000001/'
+
 files = os.listdir(path)
+files.sort()
 # only take .png files
 files_png = []
 for x in files:
@@ -14,9 +17,9 @@ for x in files:
         files_png.append(x)
 
 # index = random.randrange(0, len(files)-9)
-n_files_contained = len(files_png)-10  # so that while selecting the reward we will not heat ceiling
+n_files_contained = len(files_png)-9  # so that while selecting the reward we will not heat ceiling
 
-t_frac = 0.1
+t_frac = 1
 test_population = int(t_frac*n_files_contained)
 test_data_indices = random.sample([x for x in range(n_files_contained)], test_population)
 
@@ -27,8 +30,10 @@ seq_rewards = np.array([])
 for i in test_data_indices:  # the order of test_data_indices must not be changed while reading the frames
     frame_id = re.split("[.]", files_png[i])
     seq_id = int(frame_id[0])
-    seq_rewards=np.append(seq_rewards, rewards.iloc[seq_id+9-1])  # reward for a sequence is reaward at the 10th frame
-
+    try:
+        seq_rewards=np.append(seq_rewards, rewards.iloc[seq_id+9-1])  # reward for a sequence is reaward at the 10th frame
+    except IndexError:
+        print("frame for reward not found, index error")
 seq_rewards = seq_rewards.reshape(-1, 1)
 
 # creating the matrix from the image sequence a vector of length (5)*pixcels*3
@@ -58,7 +63,10 @@ for i in test_data_indices:
 seq_stack = seq_stack.reshape(-1, 5*frame_size)
 
 seq_stack = pd.DataFrame(seq_stack)
+seq_rewards = pd.Series(seq_rewards.flatten(), name="Y")
 
+train_XY = pd.concat((seq_stack, seq_rewards), axis=1)
+train_XY.to_csv(os.path.join(path, "trainXY.csv"))
 print(seq_stack.shape)
 
 # including the directory as well
