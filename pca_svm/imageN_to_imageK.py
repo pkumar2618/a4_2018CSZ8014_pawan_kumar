e631636de43d9c6c3a1975380afdb32c8ce73e16
Pawan Kumar <pawan.kumar@cse.iitd.ac.in>
2019-04-19 23:02:27 +0530

    able to store grayscale with its rewards, reward of frame 0 is -1

--- pca_svm/imageN_to_imageK.py
+++ pca_svm/imageN_to_imageK.py
@@ -1,9 +1,6 @@
 import os
 import re
 import cv2
-import random
-import matplotlib.pyplot as plt
-# from PIL import Image
 import numpy as np
 import pandas as pd
 
@@ -22,38 +19,37 @@ files_png = []
 for x in files:
     if x.endswith(".png"):
         files_png.append(x)
-
-
 n_files_contained = len(files_png)
 
-t_frac = 0.1
+# selecting only a fraciton of files useful during debugging
+t_frac = 0.2
 test_population = int(t_frac*n_files_contained)
-# test_data_indices = random.sample([x for x in range(n_files_contained)], test_population)
-test_data_indices = [x for x in range(n_files_contained)]
-# test_data_indices.sort()
+data_set_indices = [x for x in range(test_population)]
+
+# when selecting random files
+# data_set_indices = random.sample([x for x in range(n_files_contained)], test_population)
+# data_set_indices = [x for x in range(n_files_contained)]
 
 ### finding the rewards
-rewards = pd.read_csv(os.path.join(path,"rew.csv"), header=None)
-
+rewards = pd.read_csv(os.path.join(path, "rew.csv"), header=None)
+rewards = rewards.values
+rewards = rewards.astype('f')
 # the sequence will be identified by the last frame in the sequence,
 # here however we will only pair frames and their rewards using pandas Dataframe, with
 # the reward of the first frame being None.
 # reading all the images and storing them into a matrix of mxn dimension
 # the rows(m) are different different frames and columns are features obtained by flattening a single frame
 
-frame_rewards = np.array([], dtype=int)
-# frame_stack = np.array([], dtype=float32)
-frame_stack = np.array([],dtype='f')
+frame_rewards = np.array([])
+frame_stack = np.array([], dtype='f')
 
-for i in test_data_indices:
+for i in data_set_indices:
     frame_id = re.split("[.]", files_png[i])  # files_png might have files stored in a random order
     frame_id = int(frame_id[0])
-    try:
-        frame_rewards = np.append(frame_rewards, rewards.iloc[frame_id-1])  # reward for the is stored in rew.csv
-        # with one index less
-    except IndexError:
-        frame_rewards = np.append(frame_rewards, None)
-        print("frame for reward not found, index error")
+    if frame_id == 0:
+        frame_rewards = np.append(frame_rewards, -1)
+    else:
+        frame_rewards = np.append(frame_rewards, rewards[frame_id - 1])  # reward for the is stored in rew.csv
 
     frame = cv2.imread(os.path.join(path, files_png[i]))
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -64,14 +60,13 @@ for i in test_data_indices:
 
 frame_size = frame.size # getting the shape of the last frame
 frame_stack = frame_stack.reshape(-1, frame_size)
-
-frame_rewards = frame_rewards.reshape(-1, 1)
+frame_rewards = frame_rewards.reshape(-1, 1).astype(np.float32)
 
 # Creating a dataframe
 frame_stack = pd.DataFrame(frame_stack)
 frame_rewards = pd.Series(frame_rewards.flatten(), name="Y")
 
-train_XY = pd.concat((seq_stack, seq_rewards), axis=1)
-train_XY.to_csv(os.path.join(path, "train_XY.csv"))
+train_XY = pd.concat((frame_stack, frame_rewards), axis=1)
+train_XY.to_csv(os.path.join(path, "train_XY.csv"), index=False)
 print(frame_stack.shape)
 
