eafcb0c058d82eff9d6759d1f0e59bcd2f7e1130
pawan@phantom <43036875+pkumar2618@users.noreply.github.com>
2019-04-20 13:42:58 +0530

    running for 50 episodes

--- pca_svm/imageN_to_imageK.py
+++ pca_svm/imageN_to_imageK.py
@@ -4,15 +4,15 @@ import multiprocessing as mp
 from utils import frame_reward_to_matrix_XY
 results = []
 # path ='/Users/pawan/Documents/ml_assg/assig4/train_dataset/00000001/'
-start_path = '/Users/pawan/Documents/ml_assg/assig4/train_dataset/'
+# start_path = '/Users/pawan/Documents/ml_assg/assig4/train_dataset/'
 # path = '/home/dell/Documents/2nd_sem/ml_assig/train_dataset/00000001/'
-# start_path = '/home/dell/Documents/2nd_sem/ml_assig/train_dataset/'
+start_path = '/home/pawan/train_dataset/'
 
 dirs = next(os.walk(start_path))[1]
 # path = os.path.join(start_path, dir)
 dirs.sort()
-episodes = dirs[:4]
+episodes = dirs[:50]
 pool = mp.Pool(mp.cpu_count())
-result_objects = [pool.apply_async(frame_reward_to_matrix_XY, args=(start_path, episode_dir, 0.5)) for episode_dir in episodes]
+result_objects = [pool.apply_async(frame_reward_to_matrix_XY, args=(start_path, episode_dir, 1)) for episode_dir in episodes]
 pool.close()
 pool.join()
