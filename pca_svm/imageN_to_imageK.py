6ee9e63c932d1548db21eeffb3794d144d51270b
Pawan Kumar <pawan.kumar@cse.iitd.ac.in>
2019-04-22 16:16:25 +0530

    using svm from sklearn

--- pca_svm/imageN_to_imageK.py
+++ pca_svm/imageN_to_imageK.py
@@ -2,11 +2,13 @@ import os
 import multiprocessing as mp
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import IncrementalPCA, PCA
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, f1_score
+from sklearn import svm
+from sklearn.model_selection import learning_curve, GridSearchCV
 import numpy as np
 import pandas as pd
 
-from svmutil import *
+# from svmutil import *
 import matplotlib.pyplot as plt
 
 from utils import frame_reward_to_matrix_XY, load_all_dataset_XY
@@ -78,53 +80,100 @@ if question_part == 'd': ## parameter tunning
         print(train_XY.shape)
 
         ## creating a validation set
-        dev_set = train_XY.sample(frac=0.1, replace = False, random_state=1, axis=0)
+        dev_set = train_XY.sample(frac=0.03, replace = False, random_state=1, axis=0)
         train_XY = train_XY.drop(labels=dev_set.index)
-
-        train_X = train_XY.drop('Y',axis=1).to_numpy(copy=True)
+        # train_XY = train_XY.sample(frac=0.5, replace = False, random_state=1, axis=0)
+        train_X = train_XY.drop('Y', axis=1).to_numpy(copy=True)
         train_Y = train_XY.loc[:, 'Y'].to_numpy(copy=True)
         dev_X = dev_set.drop('Y', axis=1).to_numpy(copy=True)
         dev_Y = dev_set.loc[:, 'Y'].to_numpy(copy=True)
 
         dev_acc_lin = []
         dev_acc_gauss = []
+        dev_f1_score_lin = []
+        dev_f1_score_gauss = []
         # train_acc = []
         cost_c = [1e-5, 1e-3, 1, 5, 10]
         cwd = os.getcwd()
-        for c in cost_c:
-            # m = svm_train(train_Y, train_X_normed, '-t 0')
-            param_string_lin = '-t 2 -c %f' % c
-            param_string_gauss = '-g 0.05 -t 2 -c %f' % c
+        # for c in cost_c:
+        #     # m = svm_train(train_Y, train_X_normed, '-t 0')
+        #     param_string_lin = '-h 0 -t 0 -c %f' % c
+        #     param_string_gauss = '-h 0 -g 0.05 -t 2 -c %f' % c
+        #
+        #     m_lin = svm_train(train_Y, train_X, param_string_lin)
+        #     m_gauss = svm_train(train_Y, train_X, param_string_gauss)
+        #
+        #     p_label_lin, p_acc_lin, p_val_lin, = svm_predict(dev_Y, dev_X, m_lin)
+        #     p_label_gauss, p_acc_gauss, p_val_gauss, = svm_predict(dev_Y, dev_X, m_gauss)
+        #
+        #     dev_acc_lin.append(p_acc_lin[0])
+        #     dev_acc_gauss.append(p_acc_gauss[0])
+        #
+        #     dev_f1_score_lin.append(f1_score(dev_Y, p_label_lin, average='binary'))
+        #     dev_f1_score_gauss.append(f1_score(dev_Y, p_label_gauss, average='binary'))
+        #
+        #     svm_save_model('libsvm.model', m)
+        #     # dev_f1_score_lin.append(f1_score(dev_Y, p_label_lin, average='weighted'))
+        #     # dev_f1_score_gauss.append(f1_score(dev_Y, p_label_gauss, average='weighted'))
+        #
+        #
+        # # print('dev_acc_lin', dev_acc_lin)
+        # # print('dev_acc_gauss', dev_acc_gauss)
+        # print('dev_f1_score_lin', dev_f1_score_lin)
+        # print('dev_f1_score_gauss', dev_f1_score_gauss)
+        #
+        # # fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2)
+        # fig, (ax3, ax4) = plt.subplots(nrows=1, ncols=2)
+        # # line1 = ax1.plot(cost_c, dev_acc_lin, label='validation accuracy linear SVM with c')
+        # # ax1.legend()
+        # # ax1.set_xlabel("parameter C")
+        # # ax1.set_ylabel("accuracy")
+        # # ax1.set_title("accuracy vs C for linear SVM")
+        # #
+        # # line2 = ax2.plot(cost_c, dev_acc_gauss, label='validation accuracy with gaussian kernel with c')
+        # # ax2.legend()
+        # # ax2.set_xlabel("parameter C")
+        # # ax2.set_ylabel("accuracy")
+        # # ax2.set_title("accuracy vs C for Gaussina SVM")
+        #
+        # # f1_scores
+        # line3 = ax3.plot(cost_c, dev_f1_score_lin, label='validation f1-score linear SVM with c')
+        # ax3.legend()
+        # ax3.set_xlabel("parameter C")
+        # ax3.set_ylabel("f1-score (weighted average)")
+        # ax3.set_title("f1-score vs C for linear SVM")
+        #
+        # line4 = ax4.plot(cost_c, dev_f1_score_gauss, label='validation accuracy with gaussian kernel with c')
+        # ax4.legend()
+        # ax4.set_xlabel("parameter C")
+        # ax4.set_ylabel("f1-score (weighted average)")
+        # ax4.set_title("f1-score vs C for Gaussina SVM")
+        #
+        # plt.tight_layout()
+        # fig_name = os.path.join(cwd, "lin_gauss_svm_accuracy_with_c")
+        # plt.savefig(fig_name, format='png')
+        # plt.show()
+        parameter_candidates = [{'C': [1e-7, 1e-5, 1e-2, 1], 'kernel': ['linear']},
+                                {'C': [1e-7, 1e-5, 1e-2, 1], 'gamma': [1e-5, 1e-3], 'kernel': ['rbf']},
+                                ]
+        clf = GridSearchCV(estimator=svm.SVC(), param_grid=parameter_candidates, n_jobs=-1)
+        clf.fit(train_X, train_Y)
+        # View the accuracy score
+        print('Best score for data1:', clf.best_score_)
 
-            m_lin = svm_train(train_Y, train_X, param_string_lin)
-            m_gauss = svm_train(train_Y, train_X, param_string_gauss)
+        # View the best parameters for the model found using grid search
+        print('Best C:', clf.best_estimator_.C)
+        print('Best Kernel:', clf.best_estimator_.kernel)
+        print('Best Gamma:', clf.best_estimator_.gamma)
 
-            p_label_lin, p_acc_lin, p_val_lin, = svm_predict(dev_Y, dev_X, m_lin)
-            p_label_gauss, p_acc_gauss, p_val_gauss, = svm_predict(dev_Y, dev_X, m_gauss)
+        # train the model with new set
+        best_clf = svm.SVC(C=1e-7, kernel='linear', class_weight='balanced', random_state=0)
+        best_clf.fit(train_X, train_Y)
+        dev_mean_score = best_clf.score(dev_X, dev_Y)
+        print('dev mean accuracy', dev_mean_score)
 
-            dev_acc_lin.append(p_acc_lin[0])
-            dev_acc_gauss.append(p_acc_gauss[0])
+        pickle_store(best_clf, root_path=start_path, file_name="svm_best_clf")
 
-        print('dev_acc_lin', dev_acc_lin)
-        print('dev_acc_gauss', dev_acc_gauss)
-        # print('test_acc', test_acc)
-
-        fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
-        # ax3 = fig1.add_subplot(grid[1, :])
-
-        line1 = ax1.plot(cost_c, dev_acc_lin, label='validation accuracy linear SVM with c')
-        ax1.legend()
-        ax1.set_xlabel("parameter C")
-        ax1.set_ylabel("accuracy")
-        ax1.set_title("accuracy vs for linear SVM")
-
-        line2 = ax2.plot(cost_c, dev_acc_gauss, label='validation accuracy with gaussian kernel with c')
-        ax2.legend()
-        ax2.set_xlabel("parameter C")
-        ax2.set_ylabel("accuracy")
-        ax2.set_title("accuracy for Gaussina SVM")
-
-        plt.tight_layout()
-        fig_name = os.path.join(cwd, "lin_gauss_svm_accuracy_with_c")
-        plt.savefig(fig_name, format='png')
-        plt.show()
+        dev_Y_pred = best_clf.predict(dev_X)
+        dev_f1_score = f1_score(dev_Y, dev_Y_pred, average='binary')
+        print('dev f1-score accuracy', dev_f1_score)
